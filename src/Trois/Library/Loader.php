2a5083071dcab2a1ceaa26f3fe352ff1afbf8a7c
unknown <l>
2022-04-28 18:40:32 +0800

    获取指定标签指定属性

--- src/Trois/Library/Loader.php
+++ src/Trois/Library/Loader.php
@@ -1,10 +1,58 @@
 <?php
+
 namespace Trois\Library;
 
 class Loader
 {
-    public function index()
+    /**
+     * 匹配的字符串内容
+     * @var string
+     */
+    private $html;
+    /**
+     * 匹配的哪个标签 img a ...
+     * @var string
+     */
+    private $tag;
+
+    /**
+     * 匹配的标签的属性
+     * @var string
+     */
+    private $attr;
+
+    /**
+     * 匹配到的内容
+     * @var array
+     */
+    private $matches;
+
+    public function __construct($html, $tag = 'img', $attr = 'src')
     {
-        echo 'this is a test';
+        $this->html = $html;
+        $this->tag = $tag;
+        $this->attr = $attr;
+    }
+
+    /**
+     * 匹配符合规则的内容
+     */
+    public function getMatches()
+    {
+        $pattern = '/<' . $this->tag . '[^>]*?' . $this->attr . '="([^"]*?)"[^>]*?>/i';
+        preg_match_all($pattern, $this->html, $matches);
+        $this->matches = $matches[1];
+    }
+
+    /**
+     * 整理匹配后的结果
+     * @return array
+     */
+    public function getMatchesList()
+    {
+        foreach ($this->matches as $k => $v) {
+            $this->matches[$k] = str_replace('&#47;', '/', $v);
+        }
+        return $this->matches;
     }
 }
